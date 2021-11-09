1efb467b4e2bcee679d4cca642f820872c70644f
Stefany Eduarda <ostefany20@gmail.com>
2021-11-09 19:54:25 -0300

    Estrutura pronta #2

---
+++ src/test/java/com/db/hospedagem/AirbnbCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.hospedagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
