57585f015e4973451f87ea622ec6759bc2090dd4
danitessaro <danitcain@gmail.com>
2021-11-15 22:24:16 -0300

    Adicionando metodo findAll

--- src/main/java/com/db/hospedagem/OfertaController.java
+++ src/main/java/com/db/hospedagem/OfertaController.java
@@ -1,7 +1,10 @@
 package com.db.hospedagem;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,4 +36,9 @@ public class OfertaController {
 		repository.findAll();
 		return of;
     }
+	
+	@GetMapping("imoveis")
+	public List<Oferta> all() {
+		return (List<Oferta>) repository.findAll();
+	}
 }
